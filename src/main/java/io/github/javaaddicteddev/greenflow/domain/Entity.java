b6036b06908bd6bb47a8a28684c29b0aa877d9ce
Gabriel Valdivia <132011810+JavaAddictedDev@users.noreply.github.com>
2024-10-21 21:11:12 -0300

    feat(domain): add abstract Entity class with generic ID for domain modeling
    - created an abstract Entity class to represent base domain entities
    - added ID field to ensure all entities have an identifier
    - implemented constructor, hashCode, and equals methods to handle entity identity comparison
    - enforces non-null requirement for entity IDs

---
+++ src/main/java/io/github/javaaddicteddev/greenflow/domain/Entity.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package io.github.javaaddicteddev.greenflow.domain;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author rengoku
+ */
+public abstract class Entity<ID extends Identifier> {
+
+    protected final ID id;
+
+    protected Entity(final ID id) {
+        Objects.requireNonNull(id, "'Id' should not be null.");
+        this.id = id;
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 79 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Entity<?> other = (Entity<?>) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+}
