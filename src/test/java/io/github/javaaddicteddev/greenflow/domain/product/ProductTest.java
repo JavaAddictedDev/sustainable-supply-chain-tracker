99b0bda89d345ca1bbf698f5a5f2879381ba8fbb
Gabriel Valdivia <132011810+JavaAddictedDev@users.noreply.github.com>
2024-10-22 11:14:46 -0300

    test(product): add validation test for invalid product name
    Added a test to check the behavior when a new product is created with a null name. The test expects a DomainException to be thrown with the appropriate error message and count.

--- src/test/java/io/github/javaaddicteddev/greenflow/domain/product/ProductTest.java
+++ src/test/java/io/github/javaaddicteddev/greenflow/domain/product/ProductTest.java
@@ -1,5 +1,6 @@
 package io.github.javaaddicteddev.greenflow.domain.product;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,5 +22,25 @@ class ProductTest {
         assertEquals(expectedDescription, product.getDescription());
         assertEquals(expectedCategory, product.getCategory());
         assertEquals(expectedStackholder, product.getCurrentOwner());
+    }
+
+    @Test
+    public void shouldReceiveErrorWhenNewProductWithInvalidName() {
+        final String expectedName = null;
+        final var expectedDescription = "some descripption";
+        final var expectedCategory = "some Category";
+        final var expectedStackholder = "some stackholder";
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+
+        final var actualProduct = Product.newProduct(
+                expectedName, expectedDescription, expectedCategory, expectedStackholder);
+
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualProduct.validate());
+
+        assertNull(actualProduct);
+        assertThrows(NullPointerException.class, () -> actualProduct.getId());
+        assertEquals(expectedErrorCount, actualException.getErrors());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).getMessage());
     }
 }
